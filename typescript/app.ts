32df7127b7a6d6df4cbe0e4c62a4a66f59f25980
Kchopein <kchopein@outlook.com>
2021-03-13 22:49:33 +0100

    First compilation

---
+++ typescript/app.ts
@@ -0,0 +1,17 @@
+(function()
+{
+    function saludar( nombre: string ) {
+        console.table( 'Hola ' + nombre ); // Hola Logan
+    }
+    
+    
+    const wolverine = {
+        nombre: 'Logan'
+    };
+    
+    
+    saludar(wolverine.nombre);
+})
+
+
+
